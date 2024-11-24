a8e741772e7f95cd98f7dd5017db1352083175a2
Jacques Douglas <jacques.wsa@gmail.com>
2024-11-24 20:41:38 -0300

    Validação de preenchimento de campos

--- models/User.js
+++ models/User.js
@@ -2,14 +2,24 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: { 
+    type: String, 
+    required: [true, 'E-mail é obrigatório'], 
+    unique: true, 
+    match: [/^\S+@\S+\.\S+$/, 'E-mail inválido'], // Regex para e-mail válido
+  },
+  password: { 
+    type: String, 
+    required: [true, 'Senha é obrigatória'],
+    minlength: [6, 'A senha deve ter no mínimo 6 caracteres'], // Restrições de tamanho
+  },
 });
 
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10); // Certifica que este hash ocorre
   next();
 });
+
 
 module.exports = mongoose.model('User', userSchema);
