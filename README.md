ef7579692f7123588658fe59a091a07fa009f969
Giovanna Gonçalves <giovanna.gsouza99@gmail.com>
2024-12-03 14:36:25 -0300

    fix: implementação de telas html

---
+++ README.md
@@ -0,0 +1,33 @@
+<h1 align="center"> API REST </h1>
+
+<p align="center">
+ API Rest <br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>
+
+  
+</p>
+
+<br>
+
+
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- Express
+- Mongoose
+- MongoDB
+- Node.js
+- JWT 
+- Docker
+
+## 💻 Projeto
+
+<p>Projeto API Rest com suporte de aplicação Web com autenticação e consulta de dados.</p>
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config(); // Carregar variáveis de ambiente
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware para processar JSON
+app.use(express.json());
+
+// Validação da configuração
+if (!process.env.MONGO_URI) {
+  console.error('Erro: MONGO_URI não definido no arquivo .env');
+  process.exit(1);
+}
+
+// Conexão com o MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB conectado com sucesso!'))
+  .catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err.message);
+    process.exit(1); // Encerrar se a conexão falhar
+  });
+
+// Rotas
+app.use('/api/auth', authRoutes);
+
+// Rota inicial
+app.get('/', (req, res) => {
+  res.json({ success: true, message: 'API está funcionando' });
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
+//Verificação da saúde do Serviço - Render
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
+});
+
